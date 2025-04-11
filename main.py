9b89e18ae51bb628169c57d8542fe744e6607837
Knur324 <kszyszko99@gmail.com>
2025-04-11 09:22:54 +0200

    wip: simple data structure prepared

---
+++ main.py
@@ -0,0 +1,15 @@
+users:list = [
+    {"name":"Bernard","location":"Ełk","posts":400},
+    {"name":"Krzysztof","location":"Białobrzegi","posts":500},
+    {"name":"Maja","location":"Świecie","posts":300},
+    {"name":"Zuzanna","location":"Radzyń_Podlaski","posts":700},
+]
+
+
+
+def get_user_info(users_data:list)->None:
+
+    for user in users:
+        print(f"Twój znajomy {user['name']}! z miejscowości {user["location"]} opublikował {user['posts']} postów")
+  
+get_user_info(users)
